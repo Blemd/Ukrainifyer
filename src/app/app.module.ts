8330447a4a65da3f7544867aaf7975303c9728c1
Mondei1 <klier.nicolas@protonmail.com>
2022-03-03 17:44:59 +0100

    Add some basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarSelectorComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
